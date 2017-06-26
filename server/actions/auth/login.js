7a4f7ed5040562df2430051656e1a468ecc9edf3
Vincent OFFROY <offroy.vincent@gmail.com>
2017-06-26 17:17:22 +0200

    Middlewares
    Add of login, logout, cache, and authorizationVerification

--- server/actions/auth/login.js
+++ server/actions/auth/login.js
@@ -1,6 +1,84 @@
+const sha1 = require('sha1');
+const jwt = require('jsonwebtoken');
+
 module.exports = (api) => {
+    const User = api.models.User;
+    const Token = api.models.Token;
 
     return function login(req, res, next) {
+        if ((!req.body.mail && !req.body.userName) || !req.body.password) {
+            return res.status(500).send('no.credentials');
+        }
+        if (req.body.userName) {
+            User.findOne({
+                userName: req.body.userName,
+                password: sha1(req.body.password)
+            }, (err, user) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
 
+                if (!user) {
+                    return res.status(401).send('invalid.credentials');
+                }
+
+                var token = new Token();
+                token.userId = user._id.toString();
+
+                token.save((err, token) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+
+                    jwt.sign({
+                            exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                            tokenId: token._id.toString()
+                        },
+                        api.settings.security.salt, {}, (err, encryptedToken) => {
+                            if (err) {
+                                return res.status(500).send(err);
+                            }
+
+                            return res.send(encryptedToken);
+                        }
+                    );
+                });
+            });
+        } else {
+            User.findOne({
+                mail: req.body.mail,
+                password: sha1(req.body.password)
+            }, (err, user) => {
+                if (err) {
+                    return res.status(500).send(err);
+                }
+
+                if (!user) {
+                    return res.status(401).send('invalid.credentials');
+                }
+
+                var token = new Token();
+                token.userId = user._id.toString();
+
+                token.save((err, token) => {
+                    if (err) {
+                        return res.status(500).send(err);
+                    }
+
+                    jwt.sign({
+                            exp: Math.floor(Date.now() / 1000) + (60 * 60),
+                            tokenId: token._id.toString()
+                        },
+                        api.settings.security.salt, {}, (err, encryptedToken) => {
+                            if (err) {
+                                return res.status(500).send(err);
+                            }
+
+                            return res.send(encryptedToken);
+                        }
+                    );
+                });
+            });
+        }
     }
 };
--- server/middlewares/ensureAdmin.js
+++ server/middlewares/ensureAdmin.js
@@ -1,6 +1,20 @@
 module.exports = (api) => {
-
+    const User = api.models.User;
     return (req, res, next) => {
-        return next();
+        console.log(req.userId)
+        User.findOne({
+            _id: req.userId,
+            isAdmin: true,
+        }, (err, data) => {
+            if (err) {
+                return res.status(500).send();
+            }
+
+            if (!data) {
+                return res.status(401).send('not.admin');
+            }
+
+            return next();
+        })
     }
 }
