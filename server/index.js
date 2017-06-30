6fb42ea3e4d6fd66ed2d4434c99b3ff26233010a
Vincent OFFROY <offroy.vincent@gmail.com>
2017-06-30 11:16:51 +0200

    Global Gestion of Server V2
    minor changes

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const api = express();
 
 require('./settings')(api);
-console.log('>> Settings initialized');
+console.log('\n>> Settings initialized');
 require('./models')(api);
 console.log('>> Models initialized');
 require('./middlewares')(api);
@@ -12,6 +12,6 @@ console.log('>> Actions initialized');
 require('./routes')(api);
 console.log('>> Routes initialized');
 let date = new Date()
-console.log("(" + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + `) Server started and listening on port ${api.settings.port}`)
-console.log(`Good Job`)
 api.listen(api.settings.port);
+console.log(`(%s:%s:%s) Server started and listening on port %s`, date.getHours(), date.getMinutes(), date.getSeconds(), api.settings.port)
+console.log(`Good Job\n`)
