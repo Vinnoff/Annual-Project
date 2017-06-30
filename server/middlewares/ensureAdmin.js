84ea39ee83c43d21df5e3a4a6f578f25142e9974
Vincent OFFROY <offroy.vincent@gmail.com>
2017-06-30 11:03:42 +0200

    Global Gestion of Server
    minor changes

--- server/middlewares/ensureAdmin.js
+++ server/middlewares/ensureAdmin.js
@@ -1,20 +1,19 @@
 module.exports = (api) => {
-    const User = api.models.User;
-    return (req, res, next) => {
-        console.log(req.userId)
-        User.findOne({
-            _id: req.userId,
-            isAdmin: true,
-        }, (err, data) => {
-            if (err) {
-                return res.status(500).send();
-            }
+	const User = api.models.User;
+	return (req, res, next) => {
+		User.findOne({
+			_id: req.userId,
+			isAdmin: true,
+		}, (err, data) => {
+			if (err) {
+				return res.status(500).send();
+			}
 
-            if (!data) {
-                return res.status(401).send('not.admin');
-            }
+			if (!data) {
+				return res.status(401).send('not.admin');
+			}
 
-            return next();
-        })
-    }
+			return next();
+		})
+	}
 }
