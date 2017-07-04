43c46b798a7a165d1f207ee8cec245b716af7005
Vincent OFFROY <offroy.vincent@gmail.com>
2017-07-04 08:55:52 +0200

    Statistics suppression
    Suppression of Statistics gestion (this will be managed in the Java part)

--- server/models/Content.js
+++ server/models/Content.js
@@ -2,21 +2,17 @@ const Schema = require('mongoose').Schema;
 const timestamps = require('mongoose-timestamps');
 
 module.exports = (api) => {
-    const schema = new Schema({
-        title: {
-            type: String,
-            required: true
-        },
-        Statistics: {
-            type: Schema.Types.ObjectId,
-            ref: 'Statistics'
-        },
-        relatedRewards: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Reward'
+	const schema = new Schema({
+		title: {
+			type: String,
+			required: true
+		},
+		relatedRewards: [{
+			type: Schema.Types.ObjectId,
+			ref: 'Reward'
         }]
-    });
+	});
 
-    schema.plugin(timestamps);
-    return api.mongoose.model('Content', schema);
+	schema.plugin(timestamps);
+	return api.mongoose.model('Content', schema);
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,12 @@
 module.exports = (api) => {
-    api.use('/auth', require('./auth')(api));
-    api.use('/game', require('./game')(api));
-    api.use('/album', require('./musicalContent/album')(api));
-    api.use('/artist', require('./musicalContent/artist')(api));
-    api.use('/kind', require('./musicalContent/kind')(api));
-    api.use('/playlist', require('./musicalContent/playlist')(api));
-    api.use('/song', require('./musicalContent/song')(api));
-    api.use('/rank', require('./rank')(api));
-    api.use('/reward', require('./reward')(api));
-    api.use('/stats', require('./stats')(api));
-    api.use('/users', require('./users')(api));
+	api.use('/auth', require('./auth')(api));
+	api.use('/game', require('./game')(api));
+	api.use('/album', require('./musicalContent/album')(api));
+	api.use('/artist', require('./musicalContent/artist')(api));
+	api.use('/kind', require('./musicalContent/kind')(api));
+	api.use('/playlist', require('./musicalContent/playlist')(api));
+	api.use('/song', require('./musicalContent/song')(api));
+	api.use('/rank', require('./rank')(api));
+	api.use('/reward', require('./reward')(api));
+	api.use('/users', require('./users')(api));
 };
