2965b5c91a75bdf4ad6189f10779e7b452d12e20
maximeboinet <maximeboinet@live.com>
2017-07-05 17:12:05 +0200

    little update

--- server/actions/song/crud.js
+++ server/actions/song/crud.js
@@ -2,19 +2,71 @@ module.exports = (api) => {
     const Song = api.models.Song;
 
     function findAll(req, res, next) {
+      Song.find((err,data) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+
+        if (!data || data.lenght == 0) {
+          return res.status(204).send(data);
+        }
+
+        api.middlewares.cache.set('Produit', data, req.originalUrl);
+        return res.send(data);
+      })
 
     }
 
     function findOne(req, res, next) {
-
+      Song.findById(req.params.id, (err, data) => {
+          if (err) {
+              return res.status(500).send(err);
+          }
+          if (!data) {
+              return res.status(204).send(data);
+          }
+          return res.send(data);
+      });
     }
 
     function findByTitle(req, res, next) {
+      Produit.find({
+        title: req.params.cat,
+      }, (err, data) => {
+        if (err) {
+          return res.status(500).send();
+        }
 
+        if (!data) {
+          return res.status(204).send();
+        }
+
+        return res.send(data)
+      })
     }
 
     function create(req, res, next) {
+      let Song = new Song(req.body);
 
+      Song.findOne(req.userId, (err, user) => {
+        if (err) {
+          return res.status(500).send();
+        }
+
+        user.isVendor = true;
+        user.save((err, user) => {
+          produit.vendeur = userId;
+          produit.datemisenvente = Date.now();
+
+          produit.save((err, data) => {
+              if (err) {
+                  return res.status(500).send(err);
+              }
+
+              return res.send(data);
+          });
+        })
+      });
     }
 
     function update(req, res, next) {
