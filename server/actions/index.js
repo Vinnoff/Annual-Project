792655e1d7c60c27b3caf76987203809afe83012
maximeboinet <maximeboinet@live.com>
2017-07-08 17:01:55 +0200

    fix merge v5421.54.2147888

--- server/actions/index.js
+++ server/actions/index.js
@@ -1,19 +1,4 @@
 module.exports = (api) => {
-<<<<<<< HEAD
-  api.actions = {
-    album: require('./album/crud')(api),
-    artist: require('./artist/crud')(api),
-    auth: require('./auth')(api),
-    game: require('./game/crud')(api),
-    kind: require('./kind/crud')(api),
-    playlist: require('./playlist/crud')(api),
-    rank: require('./rank/crud')(api),
-    reward: require('./reward/crud')(api),
-    score: require('./score/crud')(api),
-    song: require('./song/crud')(api),
-    users: require('./users/crud')(api)
-  };
-=======
 	api.actions = {
 		album: require('./album/crud')(api),
 		artist: require('./artist/crud')(api),
@@ -27,5 +12,4 @@ module.exports = (api) => {
 		song: require('./song/crud')(api),
 		users: require('./users/crud')(api)
 	};
->>>>>>> f1f861554757452f9491abfd2d06991bbbf0ad40
 };
