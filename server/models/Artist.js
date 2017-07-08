21c93f0124d7747649925ecb49b1c9e7dba34331
maximeboinet <maximeboinet@live.com>
2017-07-08 16:59:36 +0200

    res con

--- server/models/Artist.js
+++ server/models/Artist.js
@@ -2,36 +2,19 @@ const Schema = require('mongoose').Schema;
 const timestamps = require('mongoose-timestamps');
 
 module.exports = (api) => {
-<<<<<<< HEAD
   const schema = new Schema({
-    title: {
-      type: String,
-      required: true
-    },
-=======
-    const schema = new Schema({
-        title: {
-            type: String,
-            required: true
-        },
-        Albums: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Album'
-        }],
-        image: {
-            type: String
-        }
+      title: {
+          type: String,
+          required: true
+      },
+      Albums: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Album'
+      }],
+      image: {
+        type: String
+      }
     });
->>>>>>> Dev_music_content
-
-    Albums: [{
-      type: Schema.Types.ObjectId,
-      ref: 'Album'
-    }],
-    image: {
-      type: String
-    }
-  });
 
   schema.plugin(timestamps);
   return api.mongoose.model('Artist', schema);
