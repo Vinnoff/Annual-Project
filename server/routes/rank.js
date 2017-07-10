01922242c05054f3c7c8647a0108417dce2c0f04
Vincent OFFROY <offroy.vincent@gmail.com>
2017-07-10 12:05:57 +0200

    Rank gestion reorganized
    Rank gestion has changed :
    - Any created rank now has a number equal to maxRank.number +1.
    - You can delete only the highest rank (use the update instead)

--- server/routes/rank.js
+++ server/routes/rank.js
@@ -1,29 +1,29 @@
 const router = require('express').Router();
 
 module.exports = (api) => {
-    router.get('/',
-        api.actions.rank.findAll);
+	router.get('/',
+		api.actions.rank.findAll);
 
-    router.get('/:id',
-        api.actions.rank.findOne);
+	router.get('/:id',
+		api.actions.rank.findById);
 
-    router.post('/',
-        api.middlewares.ensureAuthentificated,
-        api.middlewares.ensureAdmin,
-        api.middlewares.bodyParser.json(),
-        api.middlewares.cache.clean("Rank"),
-        api.actions.rank.create);
+	router.post('/',
+		api.middlewares.ensureAuthentificated,
+		api.middlewares.ensureAdmin,
+		api.middlewares.bodyParser.json(),
+		api.middlewares.cache.clean("Rank"),
+		api.actions.rank.create);
 
-    router.put('/:id',
-        api.middlewares.ensureAuthentificated,
-        api.middlewares.ensureAdmin,
-        api.middlewares.bodyParser.json(),
-        api.actions.rank.update)
+	router.put('/:id',
+		api.middlewares.ensureAuthentificated,
+		api.middlewares.ensureAdmin,
+		api.middlewares.bodyParser.json(),
+		api.actions.rank.update)
 
-    router.delete('/:id',
-        api.middlewares.ensureAuthentificated,
-        api.middlewares.ensureAdmin,
-        api.actions.rank.remove)
+	router.delete('/:id',
+		api.middlewares.ensureAuthentificated,
+		api.middlewares.ensureAdmin,
+		api.actions.rank.remove)
 
-    return router;
+	return router;
 }
