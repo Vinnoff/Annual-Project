9731d5f7564ace4460844c68d7475d63067dc6d6
Vincent OFFROY <offroy.vincent@gmail.com>
2017-07-10 16:03:03 +0200

    Temporary suspension of administrator management

--- server/middlewares/ensureAdmin.js
+++ server/middlewares/ensureAdmin.js
@@ -1,19 +1,20 @@
 module.exports = (api) => {
 	const User = api.models.User;
 	return (req, res, next) => {
-		User.findOne({
-			_id: req.userId,
-			isAdmin: true,
-		}, (err, data) => {
-			if (err) {
-				return res.status(500).send();
-			}
-
-			if (!data) {
-				return res.status(401).send('not.admin');
-			}
-
-			return next();
-		})
+		//		User.findOne({
+		//			_id: req.userId,
+		//			isAdmin: true,
+		//		}, (err, data) => {
+		//			if (err) {
+		//				return res.status(500).send();
+		//			}
+		//
+		//			if (!data) {
+		//				return res.status(401).send('not.admin');
+		//			}
+		//
+		//			return next();
+		//		})
+		return next();
 	}
 }
